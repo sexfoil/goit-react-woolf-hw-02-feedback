42b4b153a8527fac3cb26e0a5957a2d912d79cd2
Slava Poliakov <78slavapolyakov@gmail.com>
2024-02-08 18:41:58 +0200

    Finished Step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Button from './Button/Button';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateFeedback = props => {
+    this.setState(prev => {
+      return { [props.name]: prev[props.name] + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <div className={css.container}>
+          <span className={css.header}>Please leave feedback</span>
+          <div className={css['buttons-group']}>
+            <Button name={'good'} updateFeedback={this.updateFeedback} />
+            <Button name={'neutral'} updateFeedback={this.updateFeedback} />
+            <Button name={'bad'} updateFeedback={this.updateFeedback} />
+          </div>
+          <div className={css['statistic-block']}>
+            <span className={css.header}>Statistics</span>
+            <ul className={css['stat-list']}>
+              <li>Good: {this.state.good}</li>
+              <li>Neutral: {this.state.neutral}</li>
+              <li>Bad: {this.state.bad}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import css from './Button.module.css';
+
+class Button extends Component {
+  handleOnClick = () => {
+    this.props.updateFeedback(this.props);
+  };
+  render() {
+    const { name } = this.props;
+    return (
+      <button className={css.btn} type="button" onClick={this.handleOnClick}>
+        {name}
+      </button>
+    );
+  }
+}
+
+export default Button;
